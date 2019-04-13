b0e160e22e4345caba782bd9446c59e67753033f
Elise Phan <aliceconnic@gmail.com>
2019-04-13 23:47:07 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
 // Code DigitalClicker Component Here
+export default class DigitalClicker extends React.Component {
+ 
+ constructor() {
+   super()
+   this.state = {
+     timeClicked: 0
+   }
+ }
+ 
+  handleClick = () => {
+    this.setState({
+      timeClicked: this.props.timeClicked + 1
+    }, () => console.log(this.state.timeClicked)
+    )
+  }
+ 
+  render() {
+    return (
+      <button onClick={this.handleClick}>Click me!</button>
+    );
+  }
+}
