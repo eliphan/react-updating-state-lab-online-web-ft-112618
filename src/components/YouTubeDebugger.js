82eb0e7cafcb50399ea7c95d7467fb8b425657cb
Elise Phan <aliceconnic@gmail.com>
2019-04-13 23:56:21 +0000

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,34 @@
 // Code YouTubeDebugger Component Here
+export default class YouTubeDebuggerr extends React.Component {
+ 
+ constructor() {
+   super()
+   this.state = {
+     timeClicked: 0
+   }
+ }
+ 
+  handleBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      };
+    });
+  }
+  
+  handleResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        resolution: '720p'
+      };
+    });
+  }
+ 
+  render() {
+    return (
+      <button onClick={this.handleButton}>Click me!</button>
+    );
+  }
+}
