3c35f646747107aeb0c7c050242d5b92d0f80ca5
Elise Phan <aliceconnic@gmail.com>
2019-04-14 00:43:00 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,4 +1,5 @@
 // Code DigitalClicker Component Here
+<<<<<<< HEAD
 import React from 'react';
 
 export default class DigitalClicker extends React.Component {
@@ -14,11 +15,34 @@ export default class DigitalClicker extends React.Component {
     this.setState(prevState => ({
       timesClicked: prevState.timesClicked+1
     }))
+=======
+export default class DigitalClicker extends React.Component {
+ 
+ constructor() {
+   super()
+   this.state = {
+     timeClicked = 0
+   }
+ }
+ 
+  handleClick = () => {
+    this.setState({
+      timeClicked: this.props.timeClicked + 1
+    }, () => console.log(this.state.timeClicked)
+    )
+>>>>>>> 8810d97b29014d98e1db57827957cdf0ad5491bd
   }
  
   render() {
     return (
+<<<<<<< HEAD
       <button onClick={this.handleClick}>{this.state.timesClicked}</button>
     );
   }
 }  
+=======
+      <button onClick={this.handleClick}>Click me!</button>
+    );
+  }
+}
+>>>>>>> 8810d97b29014d98e1db57827957cdf0ad5491bd
